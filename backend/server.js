ad6c6ad4ece844713ede21a1a5de3a80c730e843
Kavish Tomar <kavishtomar2@gmail.com>
2023-12-28 16:15:45 +0530

    trid fixing jwt

--- backend/server.js
+++ backend/server.js
@@ -7,24 +7,15 @@ let bodyParser = require('body-parser')
 let cors = require('cors')
 let app = express();
 
-/*app.use((req, res, next) => {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });*/
+  });
 
 
 app.use(express.json());
-app.use(cors(
-    {
-        origin: [
-          "http://localhost:3000",
-          "https://kaleidoscopic-puffpuff-49f3aa.netlify.app/",
-        ],
-        credentials: true,
-      }
-));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 app.use('/api', authRoutes);
