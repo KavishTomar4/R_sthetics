35d4a6a609334858ff6c1e9fbee22233ae87593f
Kavish Tomar <kavishtomar2@gmail.com>
2023-12-29 23:05:27 +0530

    changed something

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,14 @@ let bodyParser = require('body-parser')
 let cors = require('cors')
 let app = express();
 
+
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 
-
+app.set("trust proxy", 1);
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
