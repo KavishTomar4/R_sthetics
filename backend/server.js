23dc871ba3fe035d794bb60f0edc342275dcabe2
Kavish Tomar <32963200+KavishTomar4@users.noreply.github.com>
2023-12-30 19:55:05 +0530

    removed slash

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ let app = express();
   });*/
 
 let corsOptions = {
-  origin: ['http://localhost:3000', 'https://r-sthetics-frontend.vercel.app/'],
+  origin: ['http://localhost:3000', 'https://r-sthetics-frontend.vercel.app'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // Allow cookies to be sent
 };
