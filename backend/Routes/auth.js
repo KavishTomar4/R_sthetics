1e531908b9db69f5cebc8dd9af0bf1b32fc9c96f
Kavish Tomar <kavishtomar2@gmail.com>
2023-12-31 18:43:53 +0530

    update 30

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -84,6 +84,7 @@ router.get('/login', async(req, res)=>{
 
 
     }else{
+        res.setHeader('Set-Cookie', ['type=ninja',  'language=javascript']);
         res.cookie("test", "test values", {maxAge: 30*24*60*60*1000, httpOnly: true, secure: true, sameSite: 'none', path: '/', domain: '.onrender.com'})
         res.json({toLink: ''})
     }
