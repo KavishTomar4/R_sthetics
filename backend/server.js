142635a9ced51c60292f7d8a81cb3ba86644ad84
Kavish Tomar <kavishtomar2@gmail.com>
2023-12-31 18:17:56 +0530

    update 24

--- backend/server.js
+++ backend/server.js
@@ -9,16 +9,24 @@ let cookieSession = require('cookie-session')
 let app = express();
 
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-let corsOptions = {
+  next();
+});
+
+/*let corsOptions = {
   origin: ['http://localhost:3000', 'https://rs-thetics-frontend.onrender.com'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // Allow cookies to be sent
-};
+};*/
 
 app.set("trust proxy", 1);
 app.use(express.json());
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 app.use('/api', authRoutes);
