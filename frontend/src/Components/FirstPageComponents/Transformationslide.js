3736007f856c977f23f71de72ae208a8c9883c44
Kavish Tomar <kavishtomar2@gmail.com>
2024-02-15 02:22:09 +0530

    improved UI part one

--- frontend/src/Components/FirstPageComponents/Transformationslide.js
+++ frontend/src/Components/FirstPageComponents/Transformationslide.js
@@ -7,92 +7,48 @@ import left from '../FirstPageComponents/left.png'
 import right from '../FirstPageComponents/right.png'
 import { useEffect } from "react";
 import {useState} from "react";
+import SimpleImageSlider from "react-simple-image-slider";
+
 
 
 
 function Transformationslide(){
 
-    let [leftSide, setLeft] = useState(37);
-    
-    let counter = -37;
-    let limit = -1;
-    let index = 0;
+    let [images, setImages] = useState([
+        {url: transformation_1},
+        {url: transformation_2},
+        {url: transformation_3}
+    ]);
+    let [mql, setMql] = useState(false);
     useEffect(()=>{
-        let leftSideClick = (e)=>{
-          
-          
-                counter += 18.5;
-                
-          
 
-          setLeft(counter);
-           
-              
-        }
+        setMql(window.matchMedia("(max-width: 765px)").matches);
 
-        let rightSideClick = (e)=>{
-          
-          
-            counter -= 18.5;
-            
-      
+    });
 
-            setLeft(counter);
-       
-          
-        }
-
-
-        
-        setInterval(()=>{
-           
-            
-          
-           counter += 37;
-            console.log(counter);
-
-           if(counter > 37){
-            counter = -37;
-           }
-
-           setLeft(counter);
-           limit +=1;
-           
-           
-        }, 2000);
-        
-    
-        
-        document.getElementById("leftBtn").addEventListener('click', leftSideClick);
-        document.getElementById("rightBtn").addEventListener('click', rightSideClick);
-        
-    }, [])
-  
-
+    //let [sliderId, setSliderId] = useState("image-slider")
     return(
         <div id = "transformation-slide-container">
-            <h3 style = {{marginLeft: '2.5em',marginTop: '0em', paddingTop: '1em'}}>TRANSFORMATIONS</h3>
-            <div id = "slider-container">
-            <img src = {left} width = "50" height = "50" id = "leftBtn" />
-                <div id = "transformation-slide-images">
-               
-                    <div style = {{left: leftSide.toString() + 'em' }} className = "transformation-img">
-                        <img width = "599" height = "660" src = {transformation_1}/>
-                    </div>
-                    <div  style = {{left: leftSide.toString() + 'em'}} className = "transformation-img">
-                        <img width = "599" height = "660" src = {transformation_2}/>
-                    </div>
-                    <div style = {{left: leftSide.toString() + 'em'}}  className = "transformation-img">
-                        <img width = "599" height = "660" src = {transformation_3}/>
-                    </div>
-                    
-                </div>
-            <img src = {right} width = "50" height = "50" id = "rightBtn" />
-               
-                    
-              
-               
-               
+        <h3 id = "transformation-title">TRANSFORMATIONS</h3>
+            <div id = "image-slider">
+                
+                {
+                     mql === true ? <SimpleImageSlider
+                    width={299}
+                    height= {410}
+                    images={images}
+                    showBullets={true}
+                    showNavs={true}
+                    autoPlay={true}
+                /> : <SimpleImageSlider
+                width={599}
+                height= {699}
+                images={images}
+                showBullets={true}
+                showNavs={true}
+                autoPlay={true}
+            />
+                }
             </div>
         </div>
     );
