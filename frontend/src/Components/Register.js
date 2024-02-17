8326059bd6c97a51cf72742d983138844f9b0ebd
Kavish Tomar <kavishtomar2@gmail.com>
2024-02-17 21:26:08 +0530

    Fixed Sign up registration

--- frontend/src/Components/Register.js
+++ frontend/src/Components/Register.js
@@ -48,20 +48,27 @@ function Register(){
 
     let sendDetails = async(e)=>{
         e.preventDefault();
-
-        let data = {
-            fname: document.getElementById('fname').value,
-            lname: document.getElementById('lname').value,
-            email: document.getElementById('email').value,
-            password: document.getElementById('password').value,
-            dob: document.getElementById('dob').value,
-            gender: document.getElementById('genders').value,
-            height: document.getElementById('feet').value+" feet "+document.getElementById('inches').value+" inches",
-            weight: document.getElementById('weight').value+'kgs',
-            activityType: document.getElementById('activity-types').value,
-            phonenumber: document.getElementById('phone-number').value,
-            state: document.getElementById('state').value,
-            city: document.getElementById('cities').value
+        let data;
+        if(document.getElementById('fname').value !== '' && document.getElementById('lname').value !== '' &&
+         document.getElementById('email').value !== '' && document.getElementById('password').value !== '' &&
+         document.getElementById('dob').value !== '' && document.getElementById('genders').value !== '' &&
+         document.getElementById('feet').value !== '' && document.getElementById('inches').value &&
+         document.getElementById('weight').value !== '' && document.getElementById('activity-types').value !== '' &&
+         document.getElementById('phone-number').value !== '' && document.getElementById('state').value !== '' &&
+         document.getElementById('cities').value !== ''){
+            data = {
+                fname: document.getElementById('fname').value,
+                lname: document.getElementById('lname').value,
+                email: document.getElementById('email').value,
+                password: document.getElementById('password').value,
+                dob: document.getElementById('dob').value,
+                gender: document.getElementById('genders').value,
+                height: document.getElementById('feet').value+" feet "+document.getElementById('inches').value+" inches",
+                weight: document.getElementById('weight').value+'kgs',
+                activityType: document.getElementById('activity-types').value,
+                phonenumber: document.getElementById('phone-number').value,
+                state: document.getElementById('state').value,
+                city: document.getElementById('cities').value
 
 
 
@@ -84,6 +91,12 @@ function Register(){
         }else{
             document.getElementById('error-msg').innerHTML = json.message;
         }
+    }else{
+
+        document.getElementById('error-msg').innerHTML = "Please fill the complete form";
+    }
+
+       
 
     }
 
@@ -106,18 +119,18 @@ function Register(){
         data-testid="loader"
     />:<div id = "register-form-container">
             <h3 style = {{textAlign: 'center'}}>SIGN UP</h3>
-            <p  id = "error-msg" style = {{color: 'white'}}></p>
+            <p  id = "error-msg" style = {{color: 'white', textAlign:'center'}}></p>
             <form action = "https://api.rsthetics.com/api/register" method = "POST">
                 <div id = "flnames">
-                    <input type = "text" id = "fname" placeholder="First Name" name = "fname" required/>
-                    <input type = "text" id = "lname" placeholder="Last Name" name = "lname" required/>
+                    <input type = "text" id = "fname" placeholder="First Name" name = "fname" required = {true}/>
+                    <input type = "text" id = "lname" placeholder="Last Name" name = "lname" required = {true}/>
                 </div>
-                <input type = "email" id = "email" name = "email" placeholder="E-Mail" required/>
-                <input type = "password" id = "password" name = "password" placeholder="Password" required/>
+                <input type = "email" id = "email" name = "email" placeholder="E-Mail" required = {true}/>
+                <input type = "password" id = "password" name = "password" placeholder="Password" required = {true}/>
                 <label for="dob" >Date Of Birth</label><br/>
-                <input type = "date" id = "dob" required/>
+                <input type = "date" id = "dob" required = {true}/>
                 <label htmlFor="genders" >Gender</label><br/>
-                <select  name = "genders" id = "genders" required>
+                <select  name = "genders" id = "genders" required = {true}>
                     <option value = ""><i>--Select Gender--</i></option>
                     <option value = "male">Male</option>
                     <option value = "male">Female</option>
@@ -125,20 +138,20 @@ function Register(){
                 </select>
                 <label htmlFor = "height">Height</label><br/>
                 <div id = "height">
-                    <input type = "text" id = "feet" placeholder="Feet" name = "feet" required/>
-                    <input type = "text" id = "inches" placeholder="Inches" name = "inches" required/>
+                    <input type = "text" id = "feet" placeholder="Feet" name = "feet" required = {true}/>
+                    <input type = "text" id = "inches" placeholder="Inches" name = "inches" required = {true}/>
                 </div>
-                <input type = "text" id = "weight" placeholder="Weight(in Kg)" name = "weight" required/>
+                <input type = "text" id = "weight" placeholder="Weight(in Kg)" name = "weight" required = {true}/>
                 <label htmlFor="activity-types" >Activity Type</label><br/>
-                <select  name = "activity-types" id = "activity-types" required>
+                <select  name = "activity-types" id = "activity-types" required = {true}>
                     <option value = ""><i>--Select Activity Type--</i></option>
                     <option value = "lightActivityType">Light Activity</option>
                     <option value = "moderateActivityType">Moderate Activity</option>
                     <option value = "proActivityType">Pro Activity</option>
                 </select>
-                <input type = "text" id = "phone-number" name = "phone-number" placeholder = "Phone Number" required/>
+                <input type = "text" id = "phone-number" name = "phone-number" placeholder = "Phone Number" required = {true}/>
                 <label htmlFor = "state">Select State</label><br/>
-                <select onChange={stateSelectFn}  name = "states" id = "state" required>
+                <select onChange={stateSelectFn}  name = "states" id = "state" required = {true}>
                     <option value = ""><i>--Select State--</i></option>
                     {
                         statescities.map((state_city) => {
@@ -147,7 +160,7 @@ function Register(){
                     }
                 </select>
                 <label htmlFor = "cities">Select City</label><br/>
-                <select  name = "cities" id = "cities" required>
+                <select  name = "cities" id = "cities" required = {true}>
                     <option value = ""><i>--Select City--</i></option>
                     {
                         city.map((c) => {
@@ -155,7 +168,7 @@ function Register(){
                         })
                     }
                 </select>
-                <input onClick={sendDetails} type = "submit" id = "signUp-btn" value = "Sign Up" required/>
+                <input onClick={sendDetails} type = "submit" id = "signUp-btn" value = "Sign Up" required = {true}/>
                 
 
             </form>
